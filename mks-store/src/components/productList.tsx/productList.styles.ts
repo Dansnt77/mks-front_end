fa7917601fc105cb727271e3955b030c772fdd68
Danilo <danilosnt77@gmail.com>
2024-05-29 11:37:13 -0300

    fix styles productList

--- mks-store/src/components/productList.tsx/productList.styles.ts
+++ mks-store/src/components/productList.tsx/productList.styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ProductListContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
   gap: 1rem;
   padding: 2rem;
 `;
